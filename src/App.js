18659ec0b1b5306b611b6d6afd779cacde46ded8
Toluwalope Coast <61817940+Toluwalope-Coast@users.noreply.github.com>
2022-08-28 16:31:17 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           <Route path='/'
             element={<Home />}
           />
-          <Route path='/category'
+          <Route path='/'
             element={<Category />}
           />
         </Routes>
